3d234505107adb07ecd69107052f5802678913bb
Matthew Kennedy <m.kennedy@me.com>
2023-05-10 16:27:26 +0100

    Fix flaky specs

--- spec/requests/aypex/api/v2/storefront/cms_pages_spec.rb
+++ spec/requests/aypex/api/v2/storefront/cms_pages_spec.rb
@@ -4,59 +4,39 @@ describe "Storefront API v2 CMS Pages spec" do
   let(:store) { Aypex::Store.default }
 
   before do
-    store.update(supported_locales: "en,fr")
+    store.update!(supported_locales: "en,fr")
+  end
+  after do
+    store.update!(supported_locales: "en")
   end
 
   describe "cms_pages#index" do
-    let!(:home_en) { create(:cms_homepage, store: store) }
-    let!(:standard_en) { create(:cms_standard_page, store: store, title: "About us") }
-    let!(:home_fr) { create(:cms_homepage, store: store, locale: "fr") }
-    let!(:standard_fr) { create(:cms_standard_page, store: store, locale: "fr") }
-    let!(:home_store_2) { create(:cms_homepage, store: create(:store)) }
-    let!(:page) { home_en }
-
-    shared_examples "returns proper JSON structure" do
-      it "with page attributes and relationships" do
-        page.reload
-
-        expect(json_response["data"][0]).to have_type("cms_page")
-        expect(json_response["data"][0]).to have_relationships(:cms_sections)
-        expect(json_response["data"][0]["id"]).to eq(page.id.to_s)
-        expect(json_response["data"][0]["attributes"]["title"]).to eq page.title
-        expect(json_response["data"][0]["attributes"]["locale"]).to eq page.locale
-        expect(json_response["data"][0]["attributes"]["content"]).to eq page.content
-        expect(json_response["data"][0]["attributes"]["meta_description"]).to eq page.meta_description
-        expect(json_response["data"][0]["attributes"]["meta_title"]).to eq page.meta_title
-        expect(json_response["data"][0]["attributes"]["slug"]).to eq page.slug
-        expect(json_response["data"][0]["attributes"]["type"]).to eq page.type
-      end
-    end
+    let!(:home_page_en) { create(:cms_homepage, store: store) }
+    let!(:standard_page_en) { create(:cms_standard_page, store: store, title: "About us") }
+    let!(:home_page_fr) { create(:cms_homepage, store: store, locale: "fr") }
+    let!(:standard_page_fr) { create(:cms_standard_page, store: store, locale: "fr") }
+    let!(:home_page_belonging_to_store_2) { create(:cms_homepage, store: create(:store)) }
 
     context "with no params" do
       before { get "/api/v2/storefront/cms_pages" }
 
       it_behaves_like "returns 200 HTTP status"
-      it_behaves_like "returns proper JSON structure"
-
-      it "returns all pages for the current store and locale" do
-        expect(json_response["data"].pluck(:id)).to contain_exactly(home_en.id.to_s, standard_en.id.to_s)
+      it "returns all pages for the current store matching the current locale" do
+        expect(json_response["data"].pluck(:id)).to contain_exactly(home_page_en.id.to_s, standard_page_en.id.to_s)
       end
     end
 
-    context "with locale param" do
-      let(:page) { home_fr }
-
+    context "with locale params set to fr" do
       before { get "/api/v2/storefront/cms_pages?locale=fr" }
 
       it_behaves_like "returns 200 HTTP status"
-      it_behaves_like "returns proper JSON structure"
 
       it "returns all pages for the current store and specified locale" do
-        expect(json_response["data"].pluck(:id)).to contain_exactly(home_fr.id.to_s, standard_fr.id.to_s)
+        expect(json_response["data"].pluck(:id)).to contain_exactly(home_page_fr.id.to_s, standard_page_fr.id.to_s)
       end
     end
 
-    context "filtering by type" do
+    context "when filtering by type" do
       shared_examples "returns proper records" do
         before { get "/api/v2/storefront/cms_pages?filter[type]=#{kind}" }
 
@@ -67,16 +47,16 @@ describe "Storefront API v2 CMS Pages spec" do
         end
       end
 
-      context "homepage" do
+      context "home" do
         let(:kind) { "home" }
-        let(:page) { home_en }
+        let(:page) { home_page_en }
 
         it_behaves_like "returns proper records"
       end
 
       context "standard" do
         let(:kind) { "standard" }
-        let(:page) { standard_en }
+        let(:page) { standard_page_en }
 
         it_behaves_like "returns proper records"
       end
@@ -94,7 +74,7 @@ describe "Storefront API v2 CMS Pages spec" do
         it_behaves_like "returns 200 HTTP status"
 
         it "returns all records" do
-          expect(json_response["data"].pluck(:id)).to contain_exactly(home_en.id.to_s, standard_en.id.to_s)
+          expect(json_response["data"].pluck(:id)).to contain_exactly(home_page_en.id.to_s, standard_page_en.id.to_s)
         end
       end
     end
@@ -105,22 +85,19 @@ describe "Storefront API v2 CMS Pages spec" do
       it_behaves_like "returns 200 HTTP status"
 
       it "returns page with title containing the query" do
-        expect(json_response["data"].pluck(:id)).to contain_exactly(standard_en.id.to_s)
+        expect(json_response["data"].pluck(:id)).to contain_exactly(standard_page_en.id.to_s)
       end
     end
 
-    context "including cms sections with linked resources" do
+    context "including cms_sections with cms_components" do
       let(:base_category) { create(:base_category, store: store) }
-      let!(:cms_section) { create(:cms_section_featured_article, cms_page: home_en) }
+      let!(:cms_section) { create(:cms_section_featured_article, cms_page: home_page_en) }
 
       before { get "/api/v2/storefront/cms_pages?include=cms_sections.cms_components" }
 
       it_behaves_like "returns 200 HTTP status"
-      it_behaves_like "returns proper JSON structure"
 
       it "returns sections and their associations" do
-        page.reload
-
         expect(json_response["included"]).to include(
           have_type("cms_component")
             .and(have_id(cms_section.cms_components.first.id.to_s))
@@ -141,19 +118,34 @@ describe "Storefront API v2 CMS Pages spec" do
 
   describe "cms_pages#show" do
     context "with valid page ID" do
-      let!(:page) { create(:cms_standard_page, store: store) }
-      let(:base_category) { create(:base_category, store: store) }
-      let(:category) { create(:category, base_category: base_category) }
-      let!(:page_item) { create(:cms_section_image_hero, cms_page: page) }
+      let!(:subject) { create(:cms_standard_page, store: store) }
+      let!(:base_category) { create(:base_category, store: store) }
+      let!(:category) { create(:category, base_category: base_category) }
+      let!(:page_item) { create(:cms_section_image_hero, cms_page: subject) }
 
-      before { get "/api/v2/storefront/cms_pages/#{page.id}?include=cms_sections.cms_components" }
+      before { get "/api/v2/storefront/cms_pages/#{subject.id}?include=cms_sections.cms_components" }
 
       it_behaves_like "returns 200 HTTP status"
 
-      it "returns page attributes and relationships" do
-        expect(json_response["data"]["id"]).to eq(page.id.to_s)
-        expect(json_response["data"]["attributes"]["title"]).to eq page.title
-        expect(json_response["included"]).to include(have_type("cms_section").and(have_id(page_item.id.to_s).and(have_relationship(:cms_components))))
+      it "returns correct cms_page attributes and relationships" do
+        expect(json_response["data"]).to have_type("cms_page")
+        expect(json_response["data"]).to have_relationships(:cms_sections)
+        expect(json_response["data"]["id"]).to eq(subject.id.to_s)
+        expect(json_response["data"]["attributes"]["title"]).to eq(subject.title)
+        expect(json_response["data"]["attributes"]["locale"]).to eq(subject.locale)
+        expect(json_response["data"]["attributes"]["content"]).to eq(subject.content)
+        expect(json_response["data"]["attributes"]["meta_description"]).to eq(subject.meta_description)
+        expect(json_response["data"]["attributes"]["meta_title"]).to eq(subject.meta_title)
+        expect(json_response["data"]["attributes"]["slug"]).to eq(subject.slug)
+        expect(json_response["data"]["attributes"]["type"]).to eq(subject.type)
+
+        expect(json_response["included"]).to include(
+          have_type("cms_section")
+          .and(have_id(page_item.id.to_s)
+            .and(have_relationship(:cms_components)
+              )
+            )
+          )
       end
     end
 
